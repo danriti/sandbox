1a0f869808b6274fe99a55e02d3e3674bc2cd9d4
Dan Riti <dmriti@gmail.com>
2012-09-05 22:28:21 -0400

    Update Flask app to support Shaolin.

--- hello.py
+++ hello.py
@@ -1,9 +1,30 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, jsonify
+
+from shaolin import Shaolin
+
 app = Flask(__name__)
+s = Shaolin()
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/test")
+def test():
+    return request.remote_addr
+
+@app.route("/json", methods=['POST'])
+def json():
+    if request.method == 'POST':
+        return jsonify(request.json)
+
+@app.route("/get/password")
+def password():
+    d = {'password' : s.generate_password()}
+    return jsonify(d)
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()

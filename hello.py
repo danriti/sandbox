b4598b775ca7b202186786cbba173e03d5feeaf2
Dan Riti <dmriti@gmail.com>
2012-09-06 21:29:55 -0400

    Update to support JSONP.

--- hello.py
+++ hello.py
@@ -1,11 +1,24 @@
 import json
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, current_app
+from functools import wraps
 
 from shaolin import Shaolin
 
 app = Flask(__name__)
 s = Shaolin()
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/")
 def hello():
@@ -21,9 +34,9 @@ def json():
         return jsonify(request.json)
 
 @app.route("/get/password")
+@support_jsonp
 def password():
-    d = {'password' : s.generate_password()}
-    return jsonify(d)
+    return jsonify(password=s.generate_password())
 
 if __name__ == "__main__":
     app.debug = True
